7c0f747435dfe8fe95d8cabf22eb4a53002d77c6
Pinnaker <30594400+Pinnaker@users.noreply.github.com>
2017-07-31 13:55:20 +0200

    The first step towards completing
    From today you start working on the CMS BlogMaker. The corner stone has been positioned and now we start! Of course, being a GNU project, you too have nothing to do with LItzer you can give your "help"!

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+			############################################################
+			# The following CMS was created by Litzer Tools Creator.   #
+			# The following CMS may have software defects and security #
+			# flaws. We therefore invite those who are experienced in  #
+			# web site programming to help us by creating upgrade      #
+			# patches. For more information visit:                     #
+			############################################################
+			
+			#If file 'config.php' exists run website else start installer
+			if (file_exists('config.php')){
+				
+			}else{
+				header('location: installer/index.php');
+			}
+
+?>
